810381875ed37d716cd5816393098f616f91b295
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2014-12-10 23:30:54 +0100

    added pyenv to PATH

--- includes/misc.Darwin.sh
+++ includes/misc.Darwin.sh
@@ -2,3 +2,8 @@
 if [ -a ~/.rbenv/version ]; then
     PATH="~/.rbenv/shims:~/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH"
 fi
+
+#pyenv
+if [ -a ~/.pyenv/version ]; then
+    PATH="~/.pyenv/shims:~/.pyenv/versions/$(cat ~/.pyenv/version)/bin:$PATH"
+fi
