878cbb956445ec473a4d6d3182fcf33667bd0ae2
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2014-12-21 00:08:00 +0100

    added nvm related stuff

--- includes/misc.Darwin.sh
+++ includes/misc.Darwin.sh
@@ -3,7 +3,11 @@ if [ -a ~/.rbenv/version ]; then
     PATH="$HOME/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH"
 fi
 
-#pyenv
+# pyenv
 if [ -a ~/.pyenv/version ]; then
     PATH="$HOME/.pyenv/versions/$(cat ~/.pyenv/version)/bin:$PATH"
 fi
+
+# nvm
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
