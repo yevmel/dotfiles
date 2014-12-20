407123c7320a729adc6573a4c531314006ed7b77
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2014-12-20 23:05:43 +0100

    replaced ~/ with $HOME
    in case someone (f.e. pyenv) does not expand the tilde.

--- includes/misc.Darwin.sh
+++ includes/misc.Darwin.sh
@@ -1,9 +1,9 @@
 # rbenv
 if [ -a ~/.rbenv/version ]; then
-    PATH="~/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH"
+    PATH="$HOME/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH"
 fi
 
 #pyenv
 if [ -a ~/.pyenv/version ]; then
-    PATH="~/.pyenv/versions/$(cat ~/.pyenv/version)/bin:$PATH"
+    PATH="$HOME/.pyenv/versions/$(cat ~/.pyenv/version)/bin:$PATH"
 fi
