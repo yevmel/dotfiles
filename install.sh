ab752691ab9742e647027137d4844bb65fee864f
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2015-01-06 18:34:08 +0100

    added Vundle.vim

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ for f in $(ls -A ./link); do
     LINK_FILE $PWD/link/$f $HOME/$f
 done
 
+for f in $(ls -A ./copy); do
+    COPY_FILE $PWD/copy/$f $HOME/$f
+done
+
 echo "export DOTFILES=$(pwd)" >> ~/.bashrc.custom
--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-# add stuff here that work for mac os as well as linux.
+LINK_FILE() {
+    _FROM=$1
+    _TO=$2
+
+    if [ -f $_TO ]; then
+        mv $_TO $_TO.bak
+    fi
+
+    echo "creating link from $_FROM to $_TO"
+    ln -sf $_FROM $_TO
+}
+
+COPY_FILE() {
+    _FROM=$1
+    _TO=$2
+
+    if [ -f $_TO ]; then
+        mv $_TO $_TO.bak
+    fi
+
+    echo "creating link from $_FROM to $_TO"
+    cp -r $_FROM $_TO
+}
+
+source ./setenv.sh
