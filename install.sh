de9002fda5a4efed6daca76ef78c994378faaa32
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2017-08-03 22:01:51 +0200

    added configuration for vscode.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+_OS=$(uname -r)
+
 grep -q DOTFILES ~/.bashrc
 if [ $? -eq 0 ];then
     echo "[!] ~/.bashrc already contains DOTFILES."
@@ -13,6 +15,12 @@ else
     ln -s $DOTFILES/gitignore ~/.gitignore
     ln -s $DOTFILES/gitconfig ~/.gitconfig
     ln -s $DOTFILES/editorconfig ~/.editorconfig
+
+    if [ $_OS = 'Linux' ]; then
+        echo "[!] TODO: where to putt settings.json for vscode on linux?"
+    elif [ $_OS = 'Darwin' ]; then
+        ln -s $DOTFILES/vscode-settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+    fi
 
     mkdir -p ~/.gradle && ln -s $DOTFILES/gradle.properties ~/.gradle/
 fi
