d509ee1f49bd3e46a383823949189a7095229636
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2018-04-01 00:19:23 +0200

    changed order of alias definition (alias depending on python after pyenv).

--- bashrc.d/003_misc.sh
+++ bashrc.d/003_misc.sh
@@ -1,13 +1,5 @@
 #/bin/bash
 
-PYTHON_VERSION=`python --version 2>&1`
-PYTHON_VERSION=${PYTHON_VERSION:7}
-if [[ $PYTHON_VERSION =~ ^2. ]]; then
-    alias webserver="python -m SimpleHTTPServer 8000"
-elif [[ $PYTHON_VERSION =~ ^3. ]]; then
-    alias webserver="python -m http.server 8000"
-fi
-
 # rbenv
 if [ -a ~/.rbenv/version ]; then
     PATH="$HOME/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH"
@@ -31,3 +23,10 @@ if [ -a ~/.jenv/version ]; then
     eval "$(jenv init -)";
 fi
 
+PYTHON_VERSION=`python --version 2>&1`
+PYTHON_VERSION=${PYTHON_VERSION:7}
+if [[ $PYTHON_VERSION =~ ^2. ]]; then
+    alias webserver="python -m SimpleHTTPServer 8000"
+elif [[ $PYTHON_VERSION =~ ^3. ]]; then
+    alias webserver="python -m http.server 8000"
+fi
