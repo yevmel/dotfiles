75afa12ef18391453f622ff73fdf0c3a0ef88cc5
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2018-08-31 22:19:09 +0200

    mave gdw work with path containing spaces.

--- bashrc.d/006_gradle.sh
+++ bashrc.d/006_gradle.sh
@@ -2,22 +2,24 @@
 
 # traverse the directory hierarchy up until finds a gradlew file and executes
 # it with arguments supplied by user
-gdw() {
-    _GDW_DIR=$(pwd)
-    while [ "$_GDW_DIR" != "/" ]; do
-        if [ -f "$_GDW_DIR/gradlew" ]; then
-            _GDW_EXEC="$_GDW_DIR/gradlew --stacktrace"
+_GDW_DIR="$(pwd)"
+while [ "$_GDW_DIR" != "/" ]; do
+    echo "[i] looking for gradlew in $_GDW_DIR"
 
-            if [ -f $_GDW_DIR/init.gradle ]; then
-                _GDW_EXEC="$_GDW_EXEC -I $_GDW_DIR/init.gradle"
-            fi
+    if [ -f "$_GDW_DIR/gradlew" ]; then
+        echo "[i] found gradlew in $_GDW_DIR"
 
-            $_GDW_EXEC "$@"
-            break;
+        _GDW_EXEC="$_GDW_DIR/gradlew"
+        _GDW_OPTS="--stacktrace"
+
+        if [ -f "$_GDW_DIR/init.gradle" ]; then
+            _GDW_OPTS="$_GDW_OPTS -I $_GDW_DIR/init.gradle"
         fi
 
-        _GDW_DIR=$(dirname $_GDW_DIR)
-        echo $_GDW_DIR
-    done
-}
+        "$_GDW_EXEC" $_GDW_OPTS "$@"
+        break
+    fi
+
+    _GDW_DIR=$(dirname "$_GDW_DIR")
+done
 
