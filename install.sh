377bc11245ded5bd082c88f6d3ebf3f616f86d76
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2021-05-14 15:11:21 +0200

    konsole profile

--- install.sh
+++ install.sh
@@ -19,11 +19,18 @@ else
 
     mkdir -p $HOME/.config/htop && ln -s $PWD/htoprc ~/.config/htop/htoprc
 
+    # visual studio code
     if [ $_OS = 'Linux' ]; then
-	mkdir -p ~/.config/Code/User
+        mkdir -p ~/.config/Code/User
         ln -s $PWD/vscode-settings.json ~/.config/Code/User/settings.json
     elif [ $_OS = 'Darwin' ]; then
         ln -s $PWD/vscode-settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
         ln -s $PWD/vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
+    fi
+
+    # konsole
+    if [ $_OS = 'Linux' ]; then
+        mkdir -p ~/.local/share/konsole
+        ln -s $PWD/melnichuk.profile ~/.local/share/konsole/
     fi
 fi
---
+++ melnichuk.profile
@@ -0,0 +1,17 @@
+[Appearance]
+ColorScheme=Breeze
+Font=Source Code Pro,9,-1,5,50,0,0,0,0,0
+
+[Cursor Options]
+CursorShape=1
+
+[General]
+Name=melnichuk
+Parent=FALLBACK/
+
+[Scrolling]
+HistoryMode=2
+ScrollBarPosition=2
+
+[Terminal Features]
+BlinkingCursorEnabled=true
